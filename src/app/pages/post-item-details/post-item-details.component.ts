8ddab36108eaad4be97d2590525cbf5f00555d55
hellen truong <hellentruong123@gmail.com>
2025-01-10 16:27:49 +0100

    skapar s4 sidor: home, blog, about me och post-item details

---
+++ src/app/pages/post-item-details/post-item-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-item-details',
+  imports: [],
+  templateUrl: './post-item-details.component.html',
+  styleUrl: './post-item-details.component.css'
+})
+export class PostItemDetailsComponent {
+
+}
