af5dfc24bf43e1107bd0468aab6cdf2f9cd11154
hellen truong <hellentruong123@gmail.com>
2025-01-10 18:24:50 +0100

    raderar blogg sidan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { BlogComponent } from './pages/blog/blog.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'blog', component: BlogComponent },
   { path: 'about-me', component: AboutMeComponent },
   { path: 'admin', component: AdminComponent },
 ];
