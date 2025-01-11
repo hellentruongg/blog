7b6733da785cf31b327ace360866b108c5f42b5f
hellen truong <hellentruong123@gmail.com>
2025-01-11 17:54:05 +0100

    skapar en "post" service för att hämta data

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Post } from '../models/post';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  posts: Post[] = [];
+
+  constructor() {
+    fetch('http://localhost:3000/posts')
+      .then((res) => res.json())
+      .then((data: Post[]) => (this.posts = data));
+  }
+}
