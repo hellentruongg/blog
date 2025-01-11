5f33b509f1b19dfd39139e8b3200d5980d08573f
hellen truong <hellentruong123@gmail.com>
2025-01-11 18:42:54 +0100

    monterar "posts" komponenten i "home" komponenten

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { PostsComponent } from '../../components/posts/posts.component';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
+  selector: 'home',
+  imports: [PostsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
