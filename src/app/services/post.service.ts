68a3299085f6d222e58eb593427efc77ef8b342c
hellen truong <hellentruong123@gmail.com>
2025-01-16 08:33:48 +0100

    omvandlar id från sträng till nummer

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -10,6 +10,13 @@ export class PostService {
   constructor() {
     fetch('http://localhost:3000/posts')
       .then((res) => res.json())
-      .then((data: Post[]) => (this.posts = data));
+      .then((data: Post[]) => {
+        // this.posts = data;
+        this.posts = data.map((post: any) => {
+          post.id = Number.parseInt(post.id);
+          return post;
+        });
+        console.log(data);
+      });
   }
 }
